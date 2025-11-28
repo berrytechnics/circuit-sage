11a3661e7214251362bbbaa0ed8ed089f0233ecc
Kyle Berry <kyle.berry@smartwayslfl.com>
2025-11-28 17:35:38 -0600

    Add backend/dist to .gitignore and remove tracked build artifacts

--- backend/dist/src/middlewares/index.js
+++
@@ -1,5 +0,0 @@
-export { validateRequest } from "./auth.middleware.js";
-export { requireRole, requireAdmin, requireManagerOrAdmin, requireTechnicianOrAbove, } from "./rbac.middleware.js";
-export { requireTenantContext } from "./tenant.middleware.js";
-export { validate } from "./validation.middleware.js";
-//# sourceMappingURL=index.js.map
